941ed98b884b6f62ff720f69531d616a59387fd8
Vitor Ribeiro <joao.sribeiro@aedb.br>
2025-05-18 16:55:59 -0300

    add TransitionNavigationEffect

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,9 @@ import "./globals.css";
 
 // components
 import Header from "@/components/Header";
+import Nav from "@/components/Nav";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -20,7 +23,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
+        <StairTransition />
+        <PageTransition>
         {children}
+        </PageTransition>
       </body>
       </html>
   );
