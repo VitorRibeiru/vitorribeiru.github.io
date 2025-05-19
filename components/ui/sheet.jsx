0155caafebc01631a893e59135b93e35e6e4c4c7
Vitor Ribeiro <joao.sribeiro@aedb.br>
2025-05-18 23:44:28 -0300

    add MobileNavbar component

--- components/ui/sheet.jsx
+++ components/ui/sheet.jsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { XIcon } from "lucide-react"
+import { IoMdClose } from "react-icons/io"
 
 import { cn } from "@/lib/utils"
 
@@ -54,6 +54,35 @@ function SheetContent({
   return (
     <SheetPortal>
       <SheetOverlay />
+       <SheetPrimitive.Content
+        data-slot="sheet-content"
+        className={cn(
+          "bg-primary data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-4 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+          side === "right" &&
+            "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
+          side === "left" &&
+            "data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm",
+          side === "top" &&
+            "data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b",
+          side === "bottom" &&
+            "data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom-0 h-auto border-t",
+          className
+        )}
+        {...props}>
+        <SheetPrimitive.Title className="text-lg font-semibold px-4 pt-4">
+        {/* tem que ter titulo pra não dar erro*/}
+        </SheetPrimitive.Title>
+        {children}
+        <SheetPrimitive.Close
+          className="absolute right-8 top-8 transition-opacity">
+          <IoMdClose className="text-3xl text-accent" />
+          <span className="sr-only">Close</span>
+        </SheetPrimitive.Close>
+      </SheetPrimitive.Content>
+      *
+
+
+      {/*
       <SheetPrimitive.Content
         data-slot="sheet-content"
         className={cn(
@@ -68,14 +97,15 @@ function SheetContent({
             "data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom-0 h-auto border-t",
           className
         )}
-        {...props}>
-        {children}
-        <SheetPrimitive.Close
-          className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-4" />
+        {...props}
+      >
+        <SheetPrimitive.Title className="text-lg font-semibold px-4 pt-4">
+          Título do Sheet
+        </SheetPrimitive.Title>
+
+        <IoMdClose className="text-3xl text-accent" />
           <span className="sr-only">Close</span>
-        </SheetPrimitive.Close>
-      </SheetPrimitive.Content>
+      </SheetPrimitive.Content>*/}
     </SheetPortal>
   );
 }
