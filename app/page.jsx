d6f289cad3242d07a13cf3c2cdec63a88ab2cb43
Vitor Ribeiro <joao.sribeiro@aedb.br>
2025-05-19 23:31:23 -0300

    add Banner/Social/Photo/Jobs components

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,53 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+// components
+
+import Social from "@/components/ui/Social";
+import  Jobs  from "@/components/ui/Jobs";
+import Photo from "@/components/ui/Photo";
+
 
 export default function Home() {
-  return <main>Homepage</main>;
-  
+  return (
+    <section className="h-full">
+       <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between 
+        xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">  
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent"> Vitor Ribeiro</span>
+            </h1>      
+          </div>
+          <p className="max-w-[550px] mb-9 text-white/80">
+          Eu amo a tecnologia e as coisas que ela nos permite fazer.
+          Apaixonado por jogos e café! Determinado a contribuir positivamente para o mundo!</p>
+          {/* cv and socials btn */}
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            <Button 
+              variant="outline"
+              size="lg"
+              className="uppercase flex items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FiDownload className="text-xl" />
+            </Button>
+            <div>< Social 
+            containerStyles="flex gap-6" 
+            iconStyles="w-9 h-9 border border-accent
+            rounded-full flex justify-center items-center 
+            text-accent text-base hover:bg-accent 
+            hover:text-primary hover:transition-all 
+            duration-500" 
+            />
+            </div>
+          </div>          
+          { /* photo */}
+          <Photo />
+            <Jobs />    
+        </div>
+       </div>
+    </section>
+  );
 }
---
+++ components/ui/Jobs.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+
+export const Jobs = () => {
+  const toRotate = [
+    "Fullstack Developer",
+    "Web Developer",
+    "Data Analyst",
+    "01010110 01010010"
+  ];
+
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(150);
+  const period = 1000;
+
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => clearInterval(ticker);
+  }, [text, delta]);
+
+  const tick = () => {
+    const i = loopNum % toRotate.length;
+    const fullText = toRotate[i];
+    const updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prev => prev / 1.5);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(prev => prev + 1);
+      setDelta(150);
+    }
+  };
+
+  return (
+    <section className="banner" id="home" style={{ textAlign: 'center', padding: '15px' }}>
+      <h1 className="txt-rotate" style={{ fontSize: '1rem',  minHeight: '32px' }}>
+        <span className="wrap text-xl text-accent" >{text}</span>
+      </h1>
+    </section>
+  );
+};
+    
+export default Jobs
+
+  
+
