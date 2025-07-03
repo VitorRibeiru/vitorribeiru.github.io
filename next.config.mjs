55fd508a97375cc7f81e25032cb2dba9397f2e9c
Vitor Ribeiro <102004749+jvitorribeiro@users.noreply.github.com>
2025-07-03 02:08:25 +0000

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.DEPLOY_TARGET === 'GH_PAGES';
-
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? '/vitorribeiru.github.io' : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
