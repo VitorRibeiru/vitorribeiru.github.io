f9a3e1128d9c6c42cce694254fccaac8fb085502
Vitor Ribeiro <102004749+jvitorribeiro@users.noreply.github.com>
2025-07-04 22:54:00 +0000

    update

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
 import "swiper/css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion"
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
