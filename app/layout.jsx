eb02c097084ada6307a4fe9bdd1ccab76cab3a25
Vitor Ribeiro <102004749+jvitorribeiro@users.noreply.github.com>
2025-07-06 01:42:06 +0000

    update google analytcs

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-M9HBZJLDB0"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-M9HBZJLDB0');
